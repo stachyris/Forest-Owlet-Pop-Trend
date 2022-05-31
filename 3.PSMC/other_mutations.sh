a555ca131c28cb58325695fa23bc84fe213d3c99
Vinay K L <61734552+stachyris@users.noreply.github.com>
2022-05-31 15:49:27 +0530

    Add files via upload

---
+++ 3.PSMC/other_mutations.sh
@@ -0,0 +1,6 @@
+/home/naman/soft/psmc/utils/psmc_plot.pl -R -u 2.8e-09 -g 1 FO_100x_mu1 ./FO_100x-8c.psmc
+
+/home/naman/soft/psmc/utils/psmc_plot.pl -R -u 4.6e-09 -g 1 FO_100x_mu2 ./FO_100x-8c.psmc
+
+/home/naman/soft/psmc/utils/psmc_plot.pl -R -u 1.4e-09 -g 1 FO_100x_mu3 ./FO_100x-8c.psmc
+
---
+++ 3.PSMC/text_output_noscaling.sh
@@ -0,0 +1,1 @@
+/home/naman/soft/psmc/utils/psmc_plot.pl -S -R -u 4e-09 -g 2  FO_100x_noscaling ./FO_100x-8c.psmc
